fa0a539185398ff17f44505c5dff52a2de9938b3
ms-xiaosnow <1103184133@qq.com>
2026-03-28 23:38:45 +0800

    fix: selectively preserve CODE_SIGNING_ALLOWED for RCTSwiftUI to unblock modulemap generation

--- withDisableCodeSigning.js
+++ withDisableCodeSigning.js
@@ -35,11 +35,21 @@ module.exports = function withDisableCodeSigning(config) {
   # --- INJECTED BY EXPO PLUGIN FOR UNSIGNED BUILD ---
   installer.pods_project.targets.each do |target|
     target.build_configurations.each do |config|
-      config.build_settings['CODE_SIGNING_ALLOWED'] = 'NO'
-      config.build_settings['CODE_SIGNING_REQUIRED'] = 'NO'
-      config.build_settings['EXPANDED_CODE_SIGN_IDENTITY'] = ''
-      config.build_settings['EXPANDED_CODE_SIGN_IDENTITY_NAME'] = ''
-      config.build_settings['DEVELOPMENT_TEAM'] = ''
+      if target.name == 'RCTSwiftUI'
+        # RCTSwiftUI 是纯 Swift 模块。如果禁掉 CODE_SIGNING_ALLOWED，
+        # Xcode 就不会为它生成 RCTSwiftUI.modulemap，导致 Wrapper 层报找不到文件。
+        # 解法：对它只关闭"要求"签名，但保留"允许"签名，这样 modulemap 正常输出。
+        # 同时不开 BUILD_LIBRARY_FOR_DISTRIBUTION，避免触发 SwiftVerifyEmittedModuleInterface。
+        config.build_settings['CODE_SIGNING_REQUIRED'] = 'NO'
+        config.build_settings['CODE_SIGNING_IDENTITY'] = ''
+        config.build_settings['DEVELOPMENT_TEAM'] = ''
+      else
+        config.build_settings['CODE_SIGNING_ALLOWED'] = 'NO'
+        config.build_settings['CODE_SIGNING_REQUIRED'] = 'NO'
+        config.build_settings['EXPANDED_CODE_SIGN_IDENTITY'] = ''
+        config.build_settings['EXPANDED_CODE_SIGN_IDENTITY_NAME'] = ''
+        config.build_settings['DEVELOPMENT_TEAM'] = ''
+      end
     end
   end
   # --------------------------------------------------
