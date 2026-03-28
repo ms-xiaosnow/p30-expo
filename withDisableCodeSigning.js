fa929c2232c86d1945ccd3617814d8ca2742db74
ms-xiaosnow <1103184133@qq.com>
2026-03-28 23:03:50 +0800

    fix: fully decouple codesign from pbxproj and inject explicit physical destination

--- withDisableCodeSigning.js
+++ withDisableCodeSigning.js
@@ -1,9 +1,29 @@
-const { withDangerousMod } = require('@expo/config-plugins');
+const { withDangerousMod, withXcodeProject } = require('@expo/config-plugins');
 const fs = require('fs');
 const path = require('path');
 
 module.exports = function withDisableCodeSigning(config) {
-  return withDangerousMod(config, [
+  // 1. 修改主项目的 Xcode 配置，彻底抹除签名要求
+  config = withXcodeProject(config, async (config) => {
+    const xcodeProject = config.modResults;
+    const buildConfigurations = xcodeProject.pbxXCBuildConfigurationSection();
+    for (const key in buildConfigurations) {
+      const configObj = buildConfigurations[key];
+      // 遍历所有 Build Settings
+      if (typeof configObj === 'object' && configObj.buildSettings) {
+        configObj.buildSettings['CODE_SIGNING_ALLOWED'] = '"NO"';
+        configObj.buildSettings['CODE_SIGNING_REQUIRED'] = '"NO"';
+        configObj.buildSettings['CODE_SIGNING_IDENTITY'] = '""';
+        configObj.buildSettings['EXPANDED_CODE_SIGN_IDENTITY'] = '""';
+        configObj.buildSettings['EXPANDED_CODE_SIGN_IDENTITY_NAME'] = '""';
+        configObj.buildSettings['DEVELOPMENT_TEAM'] = '""';
+      }
+    }
+    return config;
+  });
+
+  // 2. 修改底层 CocoaPods 的配置，抹除所有子依赖的签名要求
+  config = withDangerousMod(config, [
     'ios',
     async (config) => {
       const podfilePath = path.join(config.modRequest.platformProjectRoot, 'Podfile');
@@ -24,13 +44,13 @@ module.exports = function withDisableCodeSigning(config) {
   end
   # --------------------------------------------------
 `;
-      
       if (!contents.includes("CODE_SIGNING_ALLOWED")) {
          contents = contents.replace("post_install do |installer|", "post_install do |installer|" + snippet);
          fs.writeFileSync(podfilePath, contents);
       }
-      
       return config;
     },
   ]);
+  
+  return config;
 };
